5aa39db20a10bf4d6199ef31ca45ba74616ce39c
Joe McCann <joemccann@Joes-MacBook-Pro.local>
2021-05-02 00:20:53 -0500

    redux added to project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css';
 import type { AppProps /*, AppContext */ } from 'next/app';
 
+import { Provider } from 'react-redux';
+import { useStore } from '../store';
+
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	const store = useStore(pageProps.initialReduxState);
+	return (
+		<Provider store={store}>
+			<Component {...pageProps} />
+		</Provider>
+	);
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,40 @@
 import * as React from 'react';
 
+import { useSelector, useDispatch, RootStateOrAny } from 'react-redux';
+
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
-const { useState } = React;
+// const { useState } = React;
+
+const useCounter = () => {
+	const count = useSelector((state: RootStateOrAny) => state.count);
+	const dispatch = useDispatch();
+	const increment = () =>
+		dispatch({
+			type: 'INCREMENT',
+		});
+	const decrement = () =>
+		dispatch({
+			type: 'DECREMENT',
+		});
+	const reset = () =>
+		dispatch({
+			type: 'RESET',
+		});
+	return { count, increment, decrement, reset };
+};
 
 export default function Home(): JSX.Element {
-	const [count, setCounter] = useState(0);
+	const { count, increment, decrement } = useCounter();
 
-	const increaseCount = (): void => {
-		setCounter(count + 1);
-	};
+	// const increaseCount = (): void => {
+	// 	setCounter(count + 1);
+	// };
 
-	const decreaseCount = (): void => {
-		count > 0 ? setCounter(count - 1) : String;
-	};
+	// const decreaseCount = (): void => {
+	// 	count > 0 ? setCounter(count - 1) : String;
+	// };
 
 	return (
 		<div className={styles.container}>
@@ -29,10 +49,10 @@ export default function Home(): JSX.Element {
 			<main className={styles.main}>
 				<h2 className={styles.count}>{count}</h2>
 				<div className={styles.buttons}>
-					<button className={styles.button} onClick={increaseCount}>
+					<button className={styles.button} onClick={increment}>
 						Increase
 					</button>
-					<button className={styles.button} onClick={decreaseCount}>
+					<button className={styles.button} onClick={decrement}>
 						Decrease
 					</button>
 				</div>
